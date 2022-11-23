eb871c9b80661156bfb2acef9ab7ed81dbc1d6bc
Benjamin Boutelle <bennydeltoro49@gmail.com>
2022-11-23 15:05:22 -0700

    another update macy cat

--- public/main.js
+++ public/main.js
@@ -3,3 +3,17 @@ const btn = document.querySelector('button')
 const clickHandler = () => alert('The cat name is: Rascal')
 
 btn.addEventListener('click', clickHandler)
+
+baseURL = ('http://ec2-34-221-224-57.us-west-2.compute.amazonaws.com/')
+const btn2 = document.querySelector('#button2')
+
+const getCat = ( ) => {
+    axios.get(${baseURL}/api/cat)
+    .then(res => {
+        alert(res.data)
+    })
+.catch(err => {
+    console.log(err)
+})
+}
+btn2.addEventListener('click', getCat)
